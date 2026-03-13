bfa244408188d96be316b29b7fad559aa2dae6cc
Jose-1017 <j_arevalo_29@hotmail.com>
2026-03-13 10:01:05 -0500

    code

---
+++ mayor o menor.py
@@ -0,0 +1,8 @@
+p = float(input("\nEscriba el primer numero: "))
+s = float(input("\nEscriba el segundo numero: "))
+if p == s:
+        print("\nSon iguales")
+elif p > s:
+     print(f"\n{p} es mayor que {s}")
+else:
+    print(f"\n{s} es mayor que {p}")
